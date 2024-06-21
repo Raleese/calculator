8146b6b43bccf097ccdaedafb71464e9b52abc40
Titas <titaslr38@gmail.com>
2024-06-22 02:03:53 +0300

    Some logic improvements

--- scripts.js
+++ scripts.js
@@ -1,30 +1,33 @@
 function operate (operator, nr1, nr2){
+    nr1 = Number(nr1);
+    nr2 = Number(nr2);
     if (operator == "*"){
         NUM1 = nr1*nr2;
-        NUM2 = 0;
+        NUM2 = "";
     }
     else if (operator == "/"){
-        NUM1 = nr1/nr2;
-        NUM2 = 0;
+        if (nr2 == 0) nr2 = 1;
+        NUM1 = (nr1/nr2).toFixed(2);
+        NUM2 = "";
     }
     else if (operator == "+"){
         NUM1 = nr1+nr2;
-        NUM2 = 0;
+        NUM2 = "";
     }
     else if (operator == "-"){
         NUM1 = nr1-nr2;
-        NUM2 = 0;
+        NUM2 = "";
     }
     display.textContent = NUM1;
 }
 
 function clickedNum (button){
     if (OPERATOR == "NONE"){
-        NUM1 = Number(button.textContent);
+        NUM1 = NUM1 + button.textContent;
         display.textContent = NUM1;
     }
     else{
-        NUM2 = Number(button.textContent);
+        NUM2 = NUM2 + button.textContent;
         display.textContent = NUM2;
     }
 }
@@ -34,9 +37,9 @@ function clickedOperator (button){
         operate(OPERATOR, NUM1, NUM2);
     }
     else if (button.textContent == "C"){
-        NUM1 = NUM2 = 0;
+        NUM1 = NUM2 = "";
         OPERATOR = "NONE";
-        display.textContent = "0"
+        display.textContent = ""
     }
     else{
         if (OPERATOR == "NONE") OPERATOR = button.textContent;
@@ -51,8 +54,8 @@ const display = document.querySelector(".text");
 const numbers = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".action");
 
-let NUM1 = 0;
-let NUM2 = 0;
+let NUM1 = "";
+let NUM2 = "";
 let OPERATOR = "NONE";
 
 for (let i = 0; i < numbers.length; i++){
